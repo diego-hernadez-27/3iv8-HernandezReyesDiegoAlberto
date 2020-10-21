528a1fe6b7285ccaa77fa0c6da1168e1ff402de0
diego-hernandez-27 <dhernandezr1906@alumno.ipn.mx>
2020-10-21 00:55:45 -0500

    Entrada de datos usuario

--- FiguraClasica/FiguraClasica.java
+++ FiguraClasica/FiguraClasica.java
@@ -9,12 +9,15 @@ class FiguraClasica{
         int base, altura, opcion;
         char letra;
         double area, perimetro;
+
     do{
         System.out.println("Elija la opcion deseada: ");
         System.out.println("1.- Calcular el area y perimetro de un cuadrado ");
         System.out.println("2.- Calcular el area y perimetro de un triangulo ");
         System.out.println("3.- Calcular el area y perimetro de un circulo ");
-        System.out.println("4.- Salir ");
+        System.out.println("4.- Calcular el area y perimetro de un rectangulo ");
+        System.out.println("5.- Calcular el area y perimetro de un pentagono ");
+        System.out.println("6.- Salir");
 
         opcion = entrada.nextInt();
 
@@ -40,7 +43,10 @@ class FiguraClasica{
 
                 area = (base*altura)/2;
 
+                perimetro = base*3;
+
                 System.out.println("El area del triangulo es de: "+area);
+                System.out.println("Y el perimetro es:"+perimetro);
                 break;
 
             case 3:
@@ -49,11 +55,43 @@ class FiguraClasica{
 
                 area = 3.1416*base*base;
 
+                perimetro = 2*3.1416*base;
+
                 System.out.println("El area del circulo es de: "+area);
+                System.out.println("Y el perimetro es:"+perimetro);
                 break;
+            
+            case 4:
+                System.out.println("Ingrese el valor de la base ");
+                base = entrada.nextInt();
+
+                System.out.println("Ingrese el valor de la altura ");
+                altura = entrada.nextInt();
+
+                area = base*altura;
+
+                perimetro = 2*base + 2*altura;
+
+                System.out.println("El area del rectangulo es de: "+area);
+                System.out.println("Y el perimetro es: "+perimetro);
+
+            case 5:
+                System.out.println("Ingrese el valor de la base ");
+                 base = entrada.nextInt();
+
+                System.out.println("Ingrese el valor del apotema ");
+                altura = entrada.nextInt();
+
+                perimetro = base*5;
+
+                area = (perimetro*altura)/2;
+
+                System.out.println("El area del rectangulo es de: "+area);
+                System.out.println("Y el perimetro es: "+perimetro);
+
 
             default:
-                System.out.println("Gracias por participar unu nwn");
+                System.out.println("Gracias por participar uwu");
       
         }
 
